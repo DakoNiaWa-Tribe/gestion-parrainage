8978a6e167c3d67e1f1a51719252bf898a399591
Mohadevsn <mohawade23@gmail.com>
2025-03-02 15:37:07 +0000

    feat: restructure backend application and add .gitignore for environment files

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,16 @@
- 
+from fastapi import FastAPI, status, HTTPException
+from app.routers import electeur, admin, candidat, auth
+from app.database import connectionDb
+
+# Création de l'application FastAPI
+app = FastAPI()
+
+app.include_router(admin.router)
+app.include_router(candidat.router)
+app.include_router(electeur.router)
+app.include_router(auth.router)
+
+
+@app.get('/')
+def read_root():
+    return {"message": "Bienvenue sur l'API de parrainage ! La base de données est connectée."}
